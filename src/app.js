571d125d05dc664603c7004cd4f715cb037a1142
HARIKISHAN98 <harikishanmittal98@gmail.com>
2026-06-20 12:15:52 +0530

    exploring req.params, query paramter,

--- src/app.js
+++ src/app.js
@@ -2,30 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  res.send({firstName: 'John', lastName: 'Doe'});
-});
-
-app.post('/users', (req, res) => {
-  res.send('Data Successfully saved to the database');
-});
-
-app.delete('/users', (req, res) => {
-  res.send('Data Successfully deleted from the database');
-});
-
-app.put('/users', (req, res) => {
-  res.send('Data Successfully updated in the database');
-});
-
-app.patch('/users', (req, res) => {
-  res.send('Data Successfully updated in the database via patch');
-});
-
-app.use('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
+app.get("/user/:userid/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({firstName: 'Hunny', lastName: 'Mittal'});
+}); 
+ 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
