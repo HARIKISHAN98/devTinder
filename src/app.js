25734ad3338f15b2214b8dad7ffbf8ebfbcc9164
HARIKISHAN98 <harikishanmittal98@gmail.com>
2026-06-20 13:54:14 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get("/user/:userid/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({firstName: 'Hunny', lastName: 'Mittal'});
-}); 
- 
+app.use("/users", [
+  (req, res, next) => {
+    console.log("1st Route handler");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("2nd Route handler");
+      next();
+    },
+    (req, res, next) => {
+      console.log("3rd Route handler");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("4th Route handler");
+    next();
+    res.send("Hello from the users route");
+  },
+]);
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
