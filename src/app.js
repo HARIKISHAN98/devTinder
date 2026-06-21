b95e3b305a0c6c73e365d0362f75a1433c4e123c
HARIKISHAN98 <harikishanmittal98@gmail.com>
2026-06-21 10:53:58 +0530

    wildCard-error-handling

--- src/app.js
+++ src/app.js
@@ -2,34 +2,29 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/adminAuth");
-
-app.use('/admin', adminAuth);
-
-app.get("/admin/getData", (req, res) => {
-    console.log("Admin is Authorized to get data");
-    res.send("Data send to admin");
+app.use("/", (err, req, res, next) => {
+    if(err){
+        //log your error
+        res.status(500).send("Internal Server Error");  
+    }
 });
 
-app.get("/admin/deleteData", (req, res) => {
-    console.log("Admin is Authorized to delete data");
-    res.send("Data deleted from admin");
-});
- 
-app.get("/user/login", (req, res) => {
-    console.log("User is logged in successfully");
-    res.send("User logged in successfully");
-});
-
-app.get("/user/getData", userAuth, (req, res) => {
-    console.log("User is Authorized to get data");
-    res.send("Data send to user");
+app.get("/getUserData", (req, res) => {
+//    try{
+    // Logic of DB call and get User Data
+    throw new Error("hgshg");
+    res.send("User data sent");
+//    } catch (err){
+    res.status(500).send("Some Error Occured Contact Support Team");
+//    }
 });
 
-app.get("/user/deleteData", userAuth, (req, res) => {
-    console.log("User is Authorized to delete data");
-    res.send("Data deleted from user");
-}); 
+app.use("/", (err, req, res, next) => {
+    if(err){
+        //log your error
+        res.status(500).send("Internal Server Error");  
+    }
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
